35da6863559a0df83649dbd9c37c1d36fc5082ce
Kuonneko <zhu79814@sohu.com>
2015-11-19 22:37:53 +0800

    new

---
+++ mysql.php
@@ -0,0 +1,20 @@
+﻿
+
+<?php
+header("Content-Type:text/html; charset=utf-8");
+
+$link=mysql_connect("localhost","root","6126691");
+ 
+
+if(!$link) 
+    {
+    echo "ser!" ;
+    }
+else 
+    {
+    echo "成功!" ;
+    }
+ 
+
+
+?>
